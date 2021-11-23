f2869f05c7aed838389f3af46885066aa340ab0a
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2021-11-23 23:03:31 +0100

    Instance werden den Subnetzen zugeordnet und bastion erhält eine öffentliche ip

--- terraform_aws/instances.tf
+++ terraform_aws/instances.tf
@@ -1,10 +1,35 @@
+# NIC -> For subnet
+resource "aws_network_interface" "tf_bastion_instance" {
+  subnet_id   = module.vpc.public_subnets[0]
+  private_ips = ["192.168.0.10"]
+
+  tags = {
+    Name = "bastion_network_interface"
+  }
+}
+
+# NIC -> For subnet
+resource "aws_network_interface" "tf_app_instance" {
+  subnet_id   = module.vpc.private_subnets[0]
+  private_ips = ["192.168.0.100"]
+
+  tags = {
+    Name = "app_network_interface"
+  }
+}
+
 # Instance –> Amazon Linux 2 AMI (HVM) - Kernel 5.10
 resource "aws_instance" "tf_bastion_instance" {
   ami           = "ami-0bd99ef9eccfee250"
   instance_type = "t2.micro"
 
+  network_interface {
+    network_interface_id = aws_network_interface.tf_bastion_instance.id
+    device_index         = 0
+  }
+
   tags = {
-    Name = "tf_bastion-instance"
+    Name = "tf_bastion_instance"
   }
 }
 
@@ -13,7 +38,12 @@ resource "aws_instance" "tf_app_instance" {
   ami           = "ami-0bd99ef9eccfee250"
   instance_type = "t2.micro"
 
+  network_interface {
+    network_interface_id = aws_network_interface.tf_app_instance.id
+    device_index         = 0
+  }
+
   tags = {
-    Name = "tf_app-instance"
+    Name = "tf_app_instance"
   }
 }
--- terraform_aws/vpc.tf
+++ terraform_aws/vpc.tf
@@ -8,13 +8,16 @@ module "vpc" {
   private_subnets = ["192.168.0.64/26", "192.168.0.128/26"]
   public_subnets  = ["192.168.0.0/26"]
 
-  # NAT Gateways werden in jedes subnet gepackt
+  # NAT Gateways werden in jedes subnet gepackt und erhalten elastic IP's
   enable_nat_gateway = true
-  # Ein einziges NAT Gateway statt in jedem subnet & wird im ersten angegeben public subnet angelegt
+  # Ein einziges NAT Gateway statt in jedem subnet eins & wird im ersten angegeben public subnet angelegt
   single_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
+
+#! Andere Lösung wäre es kein Modul zu nehmen für VPC sondern jede Ressource einzeln
+#! https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
