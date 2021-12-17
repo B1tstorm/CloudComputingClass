6d9d6c39c1c892ba5faf590814c6ceb832747f04
anas.arodake <anas.arodake@student.fh-kiel.de>
2021-12-17 14:19:06 +0100

    trigger verbessert

--- test/parser.py
+++ test/parser.py
@@ -1,18 +1,8 @@
-#import boto3
-
-#s3 = boto3.resource('s3')
-
-#def lambda_handler(event, context):
- #   # TODO implement
-  #  data = event
-   # s3.Bucket('lab6-project-s3-target').put_object(Key='test.json', Body=data)
-
-
-
 import boto3
 import os
 import sys
 import uuid
+import json
 from urllib.parse import unquote_plus
 
 s3_client = boto3.client('s3')
@@ -21,8 +11,13 @@ s3_client = boto3.client('s3')
 def lambda_handler(event, context):
   for record in event['Records']:
       bucket = record['s3']['bucket']['name']
-      key = unquote_plus(record['s3']['object']['key'])
-      tmpkey = key.replace('/', '')
-      download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
-      s3_client.download_file(bucket, key, download_path)
-      s3_client.upload_file(download_path,"lab6-project-s3-target", key)
+      fileName = unquote_plus(record['s3']['object']['key'])
+      tempFileName = fileName.replace('/', '')
+      #ein unique dateiname wird erstellt in dem ordener temp (in der lambda)
+      file_local_path = '/tmp/{}{}'.format(uuid.uuid4(), tempFileName)
+      upload_file_name = '{}{}'.format(uuid.uuid4(), tempFileName)
+      s3_client.download_file(bucket, fileName, file_local_path)
+      s3_client.upload_file(file_local_path,"lab6-project-s3-target", upload_file_name)
+       
+      
+      
