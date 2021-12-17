3284c708c7fed865b2492bc253b93484ae650721
anas.arodake <anas.arodake@student.fh-kiel.de>
2021-12-17 11:58:26 +0100

    die parser.py nimmt jede Datei die Hochgeladen wird und speichert sie in TargetS3Bucket

--- parser.py
+++ parser.py
@@ -1,8 +1,28 @@
-import json
+#import boto3
+
+#s3 = boto3.resource('s3')
+
+#def lambda_handler(event, context):
+ #   # TODO implement
+  #  data = event
+   # s3.Bucket('lab6-project-s3-target').put_object(Key='test.json', Body=data)
+
+
+
+import boto3
+import os
+import sys
+import uuid
+from urllib.parse import unquote_plus
+
+s3_client = boto3.client('s3')
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+  for record in event['Records']:
+      bucket = record['s3']['bucket']['name']
+      key = unquote_plus(record['s3']['object']['key'])
+      tmpkey = key.replace('/', '')
+      download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
+      s3_client.download_file(bucket, key, download_path)
+      s3_client.upload_file(download_path,"lab6-project-s3-target", key)
