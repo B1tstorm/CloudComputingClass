e0620e2175b60074448c972a46cb1e0ab941130d
Jonas Hermann Becker <jonas.h.becker@student.fh-kiel.de>
2022-01-02 21:01:06 +0100

    Wechsel zwischen Components funktioniert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+export type FormType = "text" | "upload";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cc-webapp';
+
+  title = 'JSON Parser';
+
+  form: FormType = 'text';
+
+  get isTextType() {
+    return this.form === "text";
+  }
+
+  get isUploadType() {
+    return this.form === "upload";
+  }
+
+  toggleForm(type: FormType) {
+    this.form = type;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TextFormComponent } from './text-form/text-form.component';
+import { UploadFormComponent } from './upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextFormComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule
