0b1c39297274a2b452164f5bf5220b0a20600e34
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2022-01-03 16:47:26 +0100

    Auf ein s3 Bucket umgstellt

--- test/s3.tf
+++ test/s3.tf
@@ -1,23 +1,12 @@
-resource "aws_s3_bucket" "target-s3" {
-  bucket = "lab6-project-s3-target-oli"
+resource "aws_s3_bucket" "lab6-s3" {
+  bucket = "lab6-project-s3"
   acl    = "private"
   tags = {
-    Name        = "target-s3"
+    Name        = "lab6-s3"
     Environment = "Dev"
   }
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
-}
-
-
-resource "aws_s3_bucket" "source-s3" {
-  bucket = "lab6-project-s3-source-oli"
-  acl    = "private"
-  tags = {
-    Name        = "source-s3"
-    Environment = "Dev"
-  }
-
 }
