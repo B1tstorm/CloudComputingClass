49e8483eec0a9ca40be30626f45bb8004b637d8b
kamil.o.gorczyca <kamil.o.gorczyca@student.fh-kiel.de>
2022-01-10 18:40:41 +0100

    komplett neu entpackt

--- parser-lambda/parser.py
+++ parser-lambda/parser.py
@@ -4,12 +4,14 @@ import sys
 import uuid
 import json
 from urllib.parse import unquote_plus
-import pandas as pd # solution -> https://korniichuk.medium.com/lambda-with-pandas-fd81aa2ff25e
+#import pandas as pd # solution -> https://korniichuk.medium.com/lambda-with-pandas-fd81aa2ff25e
 
 s3_client = boto3.client('s3')
 
 
 def lambda_handler(event, context):
+  print("Python version: {} \nVersion info: {}").format(sys.version, sys.version_info)
+  print("OS Platform: {}").format(sys.platform)
   for record in event['Records']:
       bucket = record['s3']['bucket']['name']
       fileName = unquote_plus(record['s3']['object']['key'])
@@ -31,9 +33,11 @@ def lambda_handler(event, context):
 def convert_to_flat_csv(file_path):
   print("#####\nfile_path: " + file_path + "\n######")
   with open(file_path) as file:
+    print("with Open hat geklappt")
     df = pd.DataFrame(file)
   
-  df.to_csv("test.csv", index=False)
+  print("with open wurde verlassen")
+  df.to_csv("/tmp/test.csv", index=False)
   
   #aws lambda update-function-code --function-name parser-lambda --zip-file fileb://my-deployment-package.zip 
   #s3://lab6-project-s3/
