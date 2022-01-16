49b31290be8fa35e1802a93e0066be7bee126f67
Jonas Hermann Becker <jonas.h.becker@student.fh-kiel.de>
2022-01-16 20:52:53 +0100

    websocket funktioniert mit insomnia als trigger

--- src/app/upload-form/upload-form.component.ts
+++ src/app/upload-form/upload-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Subscription} from "rxjs";
 import {JsonResponse, UploadService} from "../upload.service";
+import {webSocket} from "rxjs/webSocket";
 
 @Component({
   selector: 'app-upload-form',
@@ -13,6 +14,8 @@ export class UploadFormComponent implements OnInit {
 
   file: File | null = null;
 
+  stringResponse: String | undefined;
+
   // This is a reference to the fileInput element in the dom, to empty its value
   // so the same file can be added twice, both times triggering the change event
   // otherwise it would not be triggered since there is not "change"
@@ -20,23 +23,33 @@ export class UploadFormComponent implements OnInit {
 
   constructor(
     private uploadService: UploadService,
-  ) { }
+  ) {
+  }
 
   onFileInput(files: FileList | null): void {
-    if(files) {
+    if (files) {
       this.file = files.item(0);
     }
   }
 
   onSubmit() {
-    if(this.file) {
+    if (this.file) {
       this.uploadService
         .uploadFile(this.file)
         .subscribe(response => {
-          this.jsonResponse = {...response}
-          this.fileInputElement.nativeElement.value = '';
-        }
-      )
+            this.jsonResponse = {...response}
+            this.fileInputElement.nativeElement.value = '';
+
+            console.log("Hier bin ich jetzt")
+
+            const subject = webSocket<String>("ws://localhost:3333/websocket?id=" + this.jsonResponse.clientId);
+            subject.subscribe(
+              msg => this.stringResponse = JSON.stringify(msg),
+              error => console.log(error),
+              () => console.log("complete")
+            )
+          }
+        )
     }
   }
 
